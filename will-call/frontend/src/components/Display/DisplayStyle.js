d645d57173635efa35677dd7d2cc26fb44b429b4
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-05-18 19:03:18 -0700

    css change

--- will-call/frontend/src/components/Display/DisplayStyle.js
+++ will-call/frontend/src/components/Display/DisplayStyle.js
@@ -4,6 +4,7 @@ export const DisplayStyle = {
     padding: 5,
     margin: 5,
     display: 'flex',
+
     flexDirection: 'row',
 
     alignItems: 'center',
@@ -17,9 +18,12 @@ export const DisplayStyle = {
     padding: 20,
     margin: 5,
     display: 'flex',
+
     flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
-    width: '45%',
+    width: '68%',
+    height: '100%',
     boxShadow: 'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset'
   },
   resultsContainer: {
@@ -115,6 +119,7 @@ export const DisplayStyle = {
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
+    // height: '100%',
     height: '85vh',
   },
 
